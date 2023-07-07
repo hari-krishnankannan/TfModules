2484b37802b8a47a1e621c450c4a0aa89149c189
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-08 00:03:35 +0530

    Update main.tf

--- network/main.tf
+++ network/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_virtual_network" "k8s" {
   name                = var.vnet_name
-  resource_group_name = azurerm_resource_group.k8s.name
+  resource_group_name = var.resource_group_name
   location            = var.location
   address_space       = var.subnet_address_space
 }
 resource "azurerm_subnet" "k8s" {
   name                             = var.subnet_name
-  resource_group_name              =  azurerm_resource_group.k8s.name
+  resource_group_name              =  var.resource_group_name
   virtual_network_name             = azurerm_virtual_network.k8s.name
   address_prefixes                 = var.subnet_address_prefixes
 }
