278258fd75b6deb5cb5ecc85971d296029b81b07
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-07 23:44:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+required_providers {
+azurerm = {
+source = “hashicorp/azurerm”
+version = “=2.46.0”
+}
+}
+}
 provider "azurerm" {
   subscription_id = var.subscription_id
   client_id       = var.client_id
