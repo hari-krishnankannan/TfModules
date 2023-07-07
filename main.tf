01506cc4911de9399246db6d21bd1c5ac45f812e
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-07 22:49:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,12 +14,13 @@ key = "ei5+DK57QOuIYBJCXra1Q7Q0ZIgBPvkKi53ODWckTE5+3E+a4aYQNjFQPGUz5CFooKGXmhGG8
 }
 }
 resource "azurerm_resource_group" "k8s" {
-  name     = var.resourcename
+  name     = var.resource_group_name
   location = var.location
 }
 module "network" {
   source    = "./network"
   vnet_name =   var.vnet_name
+resource_group_name = var.resource_group_name
    subnet_address_space    = [var.subnet_address_space]
   subnet_address_prefixes = [var.subnet_address_prefixes]
 }
