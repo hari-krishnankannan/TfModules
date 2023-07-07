ac5eb67f0aef5d92c26496b4aff3bc7f8714dc48
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-08 00:09:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "k8s" {
 module "network" {
   source    = "./network"
   vnet_name =   var.vnet_name
-resource_group_name = $(azurerm_resource_group.k8s.name)
+resource_group_name = azurerm_resource_group.k8s.name
    subnet_address_space    = [var.subnet_address_space]
   subnet_address_prefixes = [var.subnet_address_prefixes]
 }
