9923756cd77b7130239bfb453051d0c941dde8e7
root <root@Host.kh33fgqfae5ufahe2famy5xurh.rx.internal.cloudapp.net>
2023-07-07 11:30:18 +0000

    adding netwokr files

---
+++ network/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_virtual_network" "k8s" {
+  name                = var.vnet_name
+  resource_group_name = var.resource_group_name
+  location            = var.resource_group_location
+  address_space       = var.subnet_address_space
+}
+resource "azurerm_subnet" "k8s" {
+  name                             = var.subnet_name
+  resource_group_name              = var.resource_group_name
+  virtual_network_name             = azurerm_virtual_network.k8s.name
+  address_prefixes                 = var.subnet_address_prefixes
+}
