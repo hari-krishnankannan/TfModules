4817c099619ea23349a8e494c353504209f8a6d3
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-08 13:08:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,9 +24,6 @@ resource_group_name = azurerm_resource_group.k8s.name
    subnet_address_space    = [var.subnet_address_space]
   subnet_address_prefixes = [var.subnet_address_prefixes]
    }
-data "subnet" "subnet" {
-subnetid=  module.network.subnet.k8s.id
-}
 
 module "aks" {
   source              = "./aks"
@@ -34,7 +31,7 @@ module "aks" {
   location            = azurerm_resource_group.k8s.location
   cluster_name        = var.cluster_name
   vm_size             = var.vm_size
-   vnet_subnet_id     = data.subnet.subnetid
+   vnet_subnet_id     = .module.network.subnet.k8s.id
    service_cidr        = var.service_cidr
   dns_service_ip      = var.dns_service_ip
 }
