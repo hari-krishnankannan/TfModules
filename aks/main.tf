e38c020a90ec5927aace969f2e5536d784ab07d8
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-08 09:24:39 +0530

    Update main.tf

--- aks/main.tf
+++ aks/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_kubernetes_cluster" "k8s" {
-  name                = var.clustername
+  name                = var.cluster_name
   location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = var.dnspreffix
 default_node_pool {
     name       =  default
     node_count = var.agentnode
-    vm_size    = var.size
+    vm_size    = var.vm_size
     vnet_subnet_id  = var.subnet_id
   }
 network_profile {
