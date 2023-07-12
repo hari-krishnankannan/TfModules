cb5b7dc47a6f2a9acf3f64f1849dc47f1d4c32a1
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-13 00:35:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,31 @@
 provider "azurerm" {
 
-data "azurerm_client_config" "current"{
+data "azurerm_key_vault" "myscrets"{
+  name                = "myscrets"
+  resource_group_name = "Azurevms"
 }
-  subscription_id = data.azurerm_client_config.current.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-features {}
+
+data "azurerm_key_vault_secret" "clientid" {
+  name         = "clientid"
+  key_vault_id = data.azurerm_key_vault.myscrets.id
 }
+data "azurerm_key_vault_secret" "clientSecret" {
+  name         = "clientSecret"
+  key_vault_id = data.azurerm_key_vault.myscrets.id
 }
+data "azurerm_key_vault_secret" "subscriptionID" {
+  name         = "subscriptionID"
+  key_vault_id = data.azurerm_key_vault.myscrets.id
+}
+data "azurerm_key_vault_secret" "tenantid" {
+  name         = tenantid
+  key_vault_id = data.azurerm_key_vault.myscrets.id
+}
+  subscription_id = data.azurerm_key_vault_secret.subscriptionID.value
+  client_id       = data.azurerm_key_vault_secret.clientid.value
+  client_secret   = data.azurerm_key_vault_secret.clientSecret.value
+  tenant_id       = data.azurerm_key_vault_secret.tenantid.value
+
 resource "azurerm_resource_group" "k8s" {
   name     = var.resource_group_name
   location = var.location
@@ -20,16 +37,4 @@ resource_group_name = azurerm_resource_group.k8s.name
    subnet_address_space    = [var.subnet_address_space]
   subnet_address_prefixes = [var.subnet_address_prefixes]
    }
-
-module "aks" {
-  source              = "./aks"
- client_id     = var.client_id
- client_secret = var.client_secret
-  resource_group_name = azurerm_resource_group.k8s.name
-  location            = azurerm_resource_group.k8s.location
-  cluster_name        = var.cluster_name
-  vm_size             = var.vm_size
-   vnet_subnet_id     =  module.network.subnet_id
-   service_cidr        = var.service_cidr
-  dns_service_ip      = var.dns_service_ip
 }
