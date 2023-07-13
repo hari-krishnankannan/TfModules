81370749542d211fccefa11168034e121f1253de
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-13 23:48:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,24 +7,24 @@ data "azurerm_key_vault" "secrets"{
 
 data "azurerm_key_vault_secret" "id" {
   name         = "clientid"
-  key_vault_uri = https://myscrets.vault.azure.net/
+  key_vault_uri = "https://myscrets.vault.azure.net/"
 }
 data "azurerm_key_vault_secret" "Secret" {
   name         = "clientSecret"
-  key_vault_uri = https://myscrets.vault.azure.net/
+  key_vault_uri = "https://myscrets.vault.azure.net/"
 }
 data "azurerm_key_vault_secret" "subscription" {
   name         = "subscriptionid"
-  key_vault_uri = https://myscrets.vault.azure.net/
+  key_vault_uri = "https://myscrets.vault.azure.net/"
 }
 data "azurerm_key_vault_secret" "tenant" {
   name         = "tenantid"
-  key_vault_uri = https://myscrets.vault.azure.net/
+  key_vault_uri = "https://myscrets.vault.azure.net/"
 }
-  subscription_id = https://myscrets.vault.azure.net/secrets/subscriptionid/1114b528da72481a8852a19b505dfb0d
-  client_id       = https://myscrets.vault.azure.net/secrets/clientid/e5d81d51788a4daf8e64e2343df79732
-  client_secret   =https://myscrets.vault.azure.net/secrets/clientsecret/215d7086e31942919636bd190a668d7e
-  tenant_id       = https://myscrets.vault.azure.net/secrets/tenantid/79d7013f7c2049a1afbb51587682e085
+  subscription_id = "https://myscrets.vault.azure.net/secrets/subscriptionid/1114b528da72481a8852a19b505dfb0d"
+  client_id       = "https://myscrets.vault.azure.net/secrets/clientid/e5d81d51788a4daf8e64e2343df79732"
+  client_secret   = "https://myscrets.vault.azure.net/secrets/clientsecret/215d7086e31942919636bd190a668d7e"
+  tenant_id       = "https://myscrets.vault.azure.net/secrets/tenantid/79d7013f7c2049a1afbb51587682e085"
 
 resource "azurerm_resource_group" "k8s" {
   name     = var.resource_group_name
