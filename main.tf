54ce27d4054fd0d699a799a615adee25b028483f
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-14 03:52:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,18 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
 provider "azurerm" {
-features {}
-}
+
 data "azurerm_key_vault" "secret"{
   name                = "myscrets"
   resource_group_name = "Azurevms"
 }
+
+output "vault_uri" {
+
+value = data.azurerm_key_vault.secret.vault_uri
+}
+
 data "azurerm_key_vault_secret" "id" {
   name         = "clientid"
-  key_vault_id = data.azurerm_key_vault.secret.id
+  vault_uri = data.azurerm_key_vault.secret.vault_uri
 }
 
 output "client" {
@@ -25,7 +22,7 @@ value = data.azurerm_key_vault_secret.id.value
 
 data "azurerm_key_vault_secret" "clientS" {
   name         = "clientsecret"
-     key_vault_id = data.azurerm_key_vault.secret.id
+   vault_uri = data.azurerm_key_vault.secret.vault_uri
 }
 
 output "clientSecrets" {
@@ -35,7 +32,7 @@ value = data.azurerm_key_vault_secret.clientS.value
 
 data "azurerm_key_vault_secret" "subscriptionID" {
   name         = "subscriptionid"
-     key_vault_id = data.azurerm_key_vault.secret.id
+   vault_uri = data.azurerm_key_vault.secret.vault_uri
 }
 
 output "Subid" {
@@ -45,20 +42,20 @@ value = data.azurerm_key_vault_secret.subscriptionID.value
 
 data "azurerm_key_vault_secret" "tenantids" {
   name       = "tenantid"
-    key_vault_id = data.azurerm_key_vault.secret.id
+   vault_uri = data.azurerm_key_vault.secret.vault_uri
 }
 output "tenid" {
 
 value = data.azurerm_key_vault_secret.tenantids.value
 }
- 
+  subscription_id = data.azurerm_key_vault_secret.Subid
+  client_id       = data.azurerm_key_vault_secret.client
+  client_secret   = data.azurerm_key_vault_secret.clientSecrets
+  tenant_id       = data.azurerm_key_vault_secret.tenid
+
 resource "azurerm_resource_group" "k8s" {
   name     = var.resource_group_name
   location = var.location
- subscription_id = "$(data.azurerm_key_vault_secret.Subid)"
-  client_id       = "$(data.azurerm_key_vault_secret.clientSecrets)"
-  client_secret   = "$(data.azurerm_key_vault_secret.client)"
-  tenant_id       = "$(data.azurerm_key_vault_secret.tenid)"
 }
 module "network" {
   source    = "./network"
@@ -67,3 +64,4 @@ resource_group_name = azurerm_resource_group.k8s.name
    subnet_address_space    = [var.subnet_address_space]
   subnet_address_prefixes = [var.subnet_address_prefixes]
    }
+}
