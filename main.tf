afa75ced102b84143eb13f1cb0e204c7b31534a4
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-13 23:58:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
-
-  subscription_id = "https://myscrets.vault.azure.net/secrets/subscriptionid/1114b528da72481a8852a19b505dfb0d"
-  client_id       = "https://myscrets.vault.azure.net/secrets/clientid/e5d81d51788a4daf8e64e2343df79732"
-  client_secret   = "https://myscrets.vault.azure.net/secrets/clientsecret/215d7086e31942919636bd190a668d7e"
-  tenant_id       = "https://myscrets.vault.azure.net/secrets/tenantid/79d7013f7c2049a1afbb51587682e085"
-
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+features {}
+}
 resource "azurerm_resource_group" "k8s" {
   name     = var.resource_group_name
   location = var.location
@@ -16,6 +16,7 @@ resource_group_name = azurerm_resource_group.k8s.name
    subnet_address_space    = [var.subnet_address_space]
   subnet_address_prefixes = [var.subnet_address_prefixes]
    }
+
 module "aks" {
   source              = "./aks"
  client_id     = var.client_id
@@ -27,5 +28,4 @@ module "aks" {
    vnet_subnet_id     =  module.network.subnet_id
    service_cidr        = var.service_cidr
   dns_service_ip      = var.dns_service_ip
-}
 }
