4747108f4ab2150439ef14097dc844baa1e60913
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-14 01:00:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,58 @@
 provider "azurerm" {
-  subscription_id = https://myscrets.vault.azure.net/secrets/subscriptionid/1114b528da72481a8852a19b505dfb0d
-  client_id       = https://myscrets.vault.azure.net/secrets/clientid/e5d81d51788a4daf8e64e2343df79732
-  client_secret   = https://myscrets.vault.azure.net/secrets/clientsecret/215d7086e31942919636bd190a668d7e
-  tenant_id       = https://myscrets.vault.azure.net/secrets/tenantid/79d7013f7c2049a1afbb51587682e085
-features {}
+
+data "azurerm_key_vault" "secret"{
+  name                = "myscrets"
+  resource_group_name = "Azurevms"
 }
+
+output "vault_uri" {
+
+value = data.azurerm_key_vault.secret.vault_uri
+}
+
+data "azurerm_key_vault_secret" "id" {
+  name         = "clientid"
+  vault_uri = data.azurerm_key_vault.secret.vault_uri
+}
+
+output "client" {
+
+value = data.azurerm_key_vault_secret.id.value
+}
+
+data "azurerm_key_vault_secret" "clientS" {
+  name         = "clientsecret"
+   vault_uri = data.azurerm_key_vault.secret.vault_uri
+}
+
+output "clientSecrets" {
+
+value = data.azurerm_key_vault_secret.clientS.value
+}
+
+data "azurerm_key_vault_secret" "subscriptionID" {
+  name         = "subscriptionid"
+   vault_uri = data.azurerm_key_vault.secret.vault_uri
+}
+
+output "Subid" {
+
+value = data.azurerm_key_vault_secret.subscriptionID.value
+}
+
+data "azurerm_key_vault_secret" "tenantids" {
+  name       = "tenantid"
+   vault_uri = data.azurerm_key_vault.secret.vault_uri
+}
+output "tenid" {
+
+value = data.azurerm_key_vault_secret.tenantids.value
+}
+  subscription_id = data.azurerm_key_vault_secret.Subid
+  client_id       = data.azurerm_key_vault_secret.client
+  client_secret   = data.azurerm_key_vault_secret.clientSecrets
+  tenant_id       = data.azurerm_key_vault_secret.tenid
+
 resource "azurerm_resource_group" "k8s" {
   name     = var.resource_group_name
   location = var.location
@@ -16,3 +64,4 @@ resource_group_name = azurerm_resource_group.k8s.name
    subnet_address_space    = [var.subnet_address_space]
   subnet_address_prefixes = [var.subnet_address_prefixes]
    }
+}
