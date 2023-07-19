a92188987ffe56fde2704ca11259b2e8812d8dba
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-19 23:44:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,67 +1,53 @@
 provider "azurerm" {
-
-data "azurerm_key_vault" "secret"{
-  name                = "myscrets"
-  resource_group_name = "Azurevms"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+features {}
 }
-
-output "vault_uri" {
-
-value = data.azurerm_key_vault.secret.vault_uri
+terraform {
+backend "azurerm" {
+resource_group_name = "Azurevms"
+storage_account_name = "mystatefiles"
+container_name = "statefile"
+key = "ei5+DK57QOuIYBJCXra1Q7Q0ZIgBPvkKi53ODWckTE5+3E+a4aYQNjFQPGUz5CFooKGXmhGG8mZl+AStL8xSww=="
 }
-
-data "azurerm_key_vault_secret" "id" {
-  name         = "clientid"
-  vault_uri = data.azurerm_key_vault.secret.vault_uri
 }
-
-output "clientid" {
-
-value = data.azurerm_key_vault_secret.id.value
-}
-
-data "azurerm_key_vault_secret" "clientS" {
-  name         = "clientsecret"
-   vault_uri = data.azurerm_key_vault.secret.vault_uri
-}
-
-output "clientsecret" {
-
-value = data.azurerm_key_vault_secret.clientS.value
-}
-
-data "azurerm_key_vault_secret" "subscriptionID" {
-  name         = "subscriptionid"
-   vault_uri = data.azurerm_key_vault.secret.vault_uri
-}
-
-output "subscriptionid" {
-
-value = data.azurerm_key_vault_secret.subscriptionID.value
-}
-
-data "azurerm_key_vault_secret" "tenantids" {
-  name       = "tenantid"
-   vault_uri = data.azurerm_key_vault.secret.vault_uri
-}
-output "tenantid" {
-
-value = data.azurerm_key_vault_secret.tenantids.value
-}
-  subscription_id = "$(data.azurerm_key_vault_secret.subscriptionid)"
-  client_id       = "$(data.azurerm_key_vault_secret.clientid)"
-  client_secret   = "$(data.azurerm_key_vault_secret.clientsecret)"
-  tenant_id       = "$(data.azurerm_key_vault_secret.tenantid)"
-
 resource "azurerm_resource_group" "k8s" {
-  name     = var.resource_group_name
+  name     = var.resourcename
   location = var.location
 }
-module "network" {
-  source    = "./network"
-  vnet_name =   var.vnet_name
-resource_group_name = azurerm_resource_group.k8s.name
-   subnet_address_space    = [var.subnet_address_space]
-  subnet_address_prefixes = [var.subnet_address_prefixes]
-   }
+resource "azurerm_virtual_network" "k8s" {
+  name                = "k8s-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.k8s.location
+  resource_group_name = azurerm_resource_group.k8s.name
+}
+resource "azurerm_subnet" "k8s" {
+  name                 = "k8s-subnet"
+  resource_group_name  = azurerm_resource_group.k8s.name
+  virtual_network_name = azurerm_virtual_network.k8s.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = var.clustername
+  location            = azurerm_resource_group.k8s.location
+  resource_group_name = azurerm_resource_group.k8s.name
+  dns_prefix          = var.dnspreffix
+default_node_pool {
+    name       = "default"
+    node_count = var.agentnode
+    vm_size    = var.size
+    vnet_subnet_id  = azurerm_subnet.k8s.id
+  }
+network_profile {
+    network_plugin = "azure"
+    network_policy = "azure"
+    service_cidr = "10.0.2.0/24"
+    dns_service_ip = "10.0.2.10"
+  }
+service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
 }
