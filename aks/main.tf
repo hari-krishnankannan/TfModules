a20253f2b2d398520ed90cad038e8e044efaeb64
hari-krishnankannan <137082927+hari-krishnankannan@users.noreply.github.com>
2023-07-25 12:35:20 +0530

    Update main.tf

--- aks/main.tf
+++ aks/main.tf
@@ -8,6 +8,10 @@ service_principal {
   location            = var.location
   resource_group_name = var.resource_group_name
   dns_prefix          = var.dnspreffix
+
+role_based_access_control {
+    enabled = true
+  }
 default_node_pool {
     name       = "default"
     node_count = var.agentnode
